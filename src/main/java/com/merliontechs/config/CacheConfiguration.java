a693e70f30a1f0e461e9e54bf17470aae3056a1d
Gonzalo Federico Corsánigo <gonzalocor_10@hotmail.com>
2020-08-23 20:01:51 -0300

    Auto Generated Code

--- src/main/java/com/merliontechs/config/CacheConfiguration.java
+++ src/main/java/com/merliontechs/config/CacheConfiguration.java
@@ -48,6 +48,8 @@ public class CacheConfiguration {
             createCache(cm, com.merliontechs.domain.User.class.getName());
             createCache(cm, com.merliontechs.domain.Authority.class.getName());
             createCache(cm, com.merliontechs.domain.User.class.getName() + ".authorities");
+            createCache(cm, com.merliontechs.domain.Product.class.getName());
+            createCache(cm, com.merliontechs.domain.Sales.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
---
+++ src/main/java/com/merliontechs/domain/enumeration/State.java
@@ -0,0 +1,8 @@
+package com.merliontechs.domain.enumeration;
+
+/**
+ * The State enumeration.
+ */
+public enum State {
+    IN_CHARGE, SHIPPED, DELIVERED
+}
